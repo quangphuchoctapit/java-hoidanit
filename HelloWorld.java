03273a208c1e0ff5c28130eb2a1a38b892eb9ac2
Fuc le <quangphuchoctapit@gmail.com>
2024-02-25 09:18:43 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package hoidanit;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world nananananan");
+		System.out.println("Hello world nananananan");
+		System.out.println("Hello world nananananan");
+	}
+}
